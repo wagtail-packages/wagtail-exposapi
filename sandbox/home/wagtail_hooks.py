920dd961aa74e7d050ef55615a0ab05f04a5889c
Nick Moreton <nick.moreton@torchbox.com>
2024-02-16 17:15:53 +0000

    Add hooks page back to sandbox

---
+++ sandbox/home/wagtail_hooks.py
@@ -0,0 +1,45 @@
+from wagtail_modeladmin.options import ModelAdmin, modeladmin_register
+
+from .models import TestModelAdminOne, TestModelAdminThree, TestModelAdminTwo
+
+
+class TestModelAdminOneAdmin(ModelAdmin):
+    model = TestModelAdminOne
+    menu_label = "Model Admin One"
+    menu_icon = "pilcrow"
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+modeladmin_register(TestModelAdminOneAdmin)
+
+
+class TestModelAdminTwoAdmin(ModelAdmin):
+    model = TestModelAdminTwo
+    menu_label = "Model Admin Two"
+    menu_icon = "pilcrow"
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+modeladmin_register(TestModelAdminTwoAdmin)
+
+
+class TestModelAdminThreeAdmin(ModelAdmin):
+    model = TestModelAdminThree
+    menu_label = "Model Admin Three"
+    menu_icon = "pilcrow"
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+modeladmin_register(TestModelAdminThreeAdmin)
